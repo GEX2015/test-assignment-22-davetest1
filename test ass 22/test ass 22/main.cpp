06aed48077b60d73d4d2e35eb54a28bbf18fc454
Dave Burchill <dave@burchill.ca>
2016-05-08 15:40:45 -0700

    added Xcode project

---
+++ test ass 22/test ass 22/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test ass 22
+//
+//  Created by Dave Burchill on 2016-05-08.
+//  Copyright © 2016 Dave Burchill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
